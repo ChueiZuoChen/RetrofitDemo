d00a7beda6467745b7a54a1b4f1ac7268c319b23
czchen <louisa0103a@gmail.com>
2020-04-26 06:19:15 +0800

    query by path, argument post and get

--- app/src/main/java/com/example/retrofitdemo/MainActivity.kt
+++ app/src/main/java/com/example/retrofitdemo/MainActivity.kt
@@ -2,11 +2,100 @@ package com.example.retrofitdemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Observer
+import androidx.lifecycle.liveData
+import com.example.retrofitdemo.data.Albums
+import com.example.retrofitdemo.data.AlbumsItem
+import com.example.retrofitdemo.retrofit.AlbumsService
+import com.example.retrofitdemo.retrofit.RetrofitInstance
+import kotlinx.android.synthetic.main.activity_main.*
+import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
-
+    private lateinit var retrofitService: AlbumsService
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        retrofitService = RetrofitInstance
+            .getRetrofitInstance()
+            .create(AlbumsService::class.java)
+
+//        getRequestWithPathParameters()
+//        getRequestWithQueryParameters()
+        uploadAlbums()
+
+
     }
+
+
+    private fun getRequestWithQueryParameters() {
+        val responseLiveData: LiveData<Response<Albums>> = liveData {
+//            val response: Response<Albums> = retrofitService.getAlbums()
+            val response = retrofitService.getSortedAlbums(7)
+            emit(response)
+        }
+
+        responseLiveData.observe(this, Observer {
+            val albumsList = it.body()?.listIterator()
+            if (albumsList != null) {
+                while (albumsList.hasNext()) {
+                    val albumsItem = albumsList.next()
+//                    Log.d("MYTAG", albumsItem.title)
+
+                    val result = " " + "Albmus title : ${albumsItem.title} \n" +
+                            " " + "Albmus id : ${albumsItem.id} \n" +
+                            " " + "Albmus userId : ${albumsItem.userId} \n\n"
+
+                    tv_1.append(result)
+                }
+            }
+        })
+    }
+
+    private fun getRequestWithPathParameters() {
+        //path parameter example
+        val pathResponse: LiveData<Response<AlbumsItem>> = liveData {
+            val response = retrofitService.getAlbum(3)
+            emit(response)
+        }
+
+        pathResponse.observe(this, Observer {
+            val title: String? = it.body()?.title
+            Toast.makeText(applicationContext, title, Toast.LENGTH_LONG).show()
+        })
+    }
+
+    private fun uploadAlbums(){
+        val albums = AlbumsItem(10,"ddd",10)
+        val postResponse:LiveData<Response<AlbumsItem>> = liveData {
+            val response:Response<AlbumsItem> = retrofitService.uploadAlbum(albums)
+            emit(response)
+        }
+        postResponse.observe(this, Observer {
+            val receivedAlbumsItem = it.body()
+            val result = " " + "Albmus title : ${receivedAlbumsItem?.title} \n" +
+                    " " + "Albmus id : ${receivedAlbumsItem?.id} \n" +
+                    " " + "Albmus userId : ${receivedAlbumsItem?.userId} \n\n"
+            tv_1.text = result
+        })
+    }
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
--- app/src/main/java/com/example/retrofitdemo/retrofit/AlbumsService.kt
+++ app/src/main/java/com/example/retrofitdemo/retrofit/AlbumsService.kt
@@ -1,9 +1,23 @@
 package com.example.retrofitdemo.retrofit
 
 import com.example.retrofitdemo.data.Albums
+import com.example.retrofitdemo.data.AlbumsItem
 import retrofit2.Response
+import retrofit2.http.*
 
 interface AlbumsService {
-
+    //    https://jsonplaceholder.typicode.com/albums
+    @GET("/albums")
     suspend fun getAlbums(): Response<Albums>
+
+    //    https://jsonplaceholder.typicode.com/albums?userId=3
+    @GET("/albums")
+    suspend fun getSortedAlbums(@Query("userId") userId: Int): Response<Albums>
+
+    //    https://jsonplaceholder.typicode.com/albums/3
+    @GET("/albums/{id}")
+    suspend fun getAlbum(@Path(value = "id") id: Int): Response<AlbumsItem>
+
+    @POST("/albums")
+    suspend fun uploadAlbum(@Body album:AlbumsItem): Response<AlbumsItem>
 }
